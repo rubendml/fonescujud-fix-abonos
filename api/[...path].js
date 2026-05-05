afcf87758bb1f4c68f01ce4c1ed0a1cebc87efee
rubendml <rubendml@gmail.com>
2026-05-05 15:08:58 -0500

    fix cors definitivo

--- api/[...path].js
+++ api/[...path].js
@@ -8,6 +8,7 @@ export default function handler(req, res) {
     res.setHeader('Access-Control-Allow-Methods', 'GET,POST,PUT,PATCH,DELETE,OPTIONS');
     res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
 
+    // 🔥 ESTE ES EL FIX REAL
     if (req.method === 'OPTIONS') {
         return res.status(200).end();
     }
